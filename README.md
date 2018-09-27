70b1e4d409f300f36ce933961e52366256d8042d
Hendi Santika <hendisantika@gmail.com>
2018-09-28 06:52:30 +0700

    #1 - Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Coba H2
+
+Now we should copy URL (from sample App output) and credentials sa/sa to login into Console
+URL: jdbc:h2:tcp://192.168.1.100:9092/mem:mytest
+Username: sa
+Password: sa# coba-h2
---
+++ src/main/java/com/hendisantika/h2/H2DatabaseViewerExample.java
@@ -0,0 +1,88 @@
+package com.hendisantika.h2;
+
+import org.h2.tools.Server;
+
+import java.sql.*;
+
+/**
+ * Created by hendisantika on 4/30/17.
+ */
+public class H2DatabaseViewerExample {
+    private static final String DB_DRIVER = "org.h2.Driver";
+    private static final String DB_CONNECTION = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
+    private static final String DB_USER = "sa";
+    private static final String DB_PASSWORD = "sa";
+
+    public static void main(String[] args) {
+        try {
+            openServerModeInBrowser();
+            insertWithPreparedStatement();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void insertWithPreparedStatement() throws SQLException {
+        Connection connection = getDBConnection();
+        PreparedStatement createPreparedStatement = null;
+        PreparedStatement insertPreparedStatement = null;
+        PreparedStatement selectPreparedStatement = null;
+
+        String CreateQuery = "CREATE TABLE PERSON(id int primary key, name varchar(255))";
+        String InsertQuery = "INSERT INTO PERSON" + "(id, name) values" + "(?,?)";
+        String SelectQuery = "select * from PERSON";
+
+        try {
+            connection.setAutoCommit(false);
+
+            createPreparedStatement = connection.prepareStatement(CreateQuery);
+            createPreparedStatement.executeUpdate();
+            createPreparedStatement.close();
+
+            insertPreparedStatement = connection.prepareStatement(InsertQuery);
+            insertPreparedStatement.setInt(1, 1);
+            insertPreparedStatement.setString(2, "Hendi Santika");
+            insertPreparedStatement.executeUpdate();
+            insertPreparedStatement.close();
+
+            selectPreparedStatement = connection.prepareStatement(SelectQuery);
+            ResultSet rs = selectPreparedStatement.executeQuery();
+            System.out.println("H2 In-Memory Database inserted through PreparedStatement");
+            while (rs.next()) {
+                System.out.println("Id " + rs.getInt("id") + " Name " + rs.getString("name"));
+            }
+            selectPreparedStatement.close();
+
+            connection.commit();
+        } catch (SQLException e) {
+            System.out.println("Exception Message " + e.getLocalizedMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            connection.close();
+        }
+    }
+
+    private static Connection getDBConnection() {
+        Connection dbConnection = null;
+        try {
+            Class.forName(DB_DRIVER);
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
+            return dbConnection;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return dbConnection;
+    }
+
+    private static void openServerModeInBrowser() throws SQLException {
+        Server server = Server.createTcpServer().start();
+        System.out.println("Server started and connection is open.");
+        System.out.println("URL: jdbc:h2:" + server.getURL() + "/mem:test");
+    }
+}
